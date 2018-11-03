1e1dfaca391d535ba2b12329f42687a17b6ec720
Andy Mender <andymenderunix@gmail.com>
2018-11-03 11:03:19 +0100

    Add path constants to libs.constants

--- libs/constants.py
+++ libs/constants.py
@@ -5,6 +5,17 @@
     date: 2018-11-02
 """
 
+import os.path
+import pathlib
+
 # pygame constants
 SCREEN_SIZE = (600, 400)
 TITLE_BAR = 'Johny Underwater'
+
+# path constants
+BASEDIR = os.path.join(*pathlib.Path(os.path.realpath(__file__)).parts[:-2])
+ASSETS_DIR = os.path.join(BASEDIR, "assets")
+MAP_DIR = os.path.join(ASSETS_DIR, "maps")
+SPRITE_DIR = os.path.join(ASSETS_DIR, "sprites")
+SFX_DIR = os.path.join(ASSETS_DIR, "sfx")
+MUSIC_DIR = os.path.join(ASSETS_DIR, "music")
