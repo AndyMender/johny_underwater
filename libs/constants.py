acee0fe9f3552cadb5f769c267ffadd36dcfe0a4
Andy Mender <andymenderunix@gmail.com>
2018-11-03 10:34:20 +0100

    Add basic constants lib

---
+++ libs/constants.py
@@ -0,0 +1,10 @@
+"""
+    Constants and static parameters used by the game
+
+    author: Andy Mender <andymenderunix@gmail.com>
+    date: 2018-11-02
+"""
+
+# pygame constants
+SCREEN_SIZE = (600, 400)
+TITLE_BAR = 'Johny Underwater'
