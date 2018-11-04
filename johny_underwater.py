4d886a913714c2a2f7178cf47bff2bed93aef3c0
Andy Mender <andymenderunix@gmail.com>
2018-11-04 16:47:52 +0100

    Add bubbles SVG and PNG animation frames

--- johny_underwater.py
+++ johny_underwater.py
@@ -1,14 +1,42 @@
-from libs.engine import Engine
+import logging
+import os.path
+import sys
+
+from libs.constants import (MAP_DIR, PYGAME_ERROR, PYGAME_FAILED,
+                            PYGAME_SUCCESS, SPRITE_DIR)
+from libs.engine import game_engine
+from libs.entity import AnimEntity
+
+# set up main logger
+logger = logging.getLogger(__file__)
 
 if __name__ == '__main__':
 
-    # load and start game engine
-    game_engine = Engine()
+    # start game engine and load elements
+    game_engine.init()
 
-    game_engine.init_pygame()
+    game_engine.load_map(os.path.join(MAP_DIR, "project1.tmx"))
 
-    game_engine.pg.draw.rect(game_engine.SCREEN, (0, 120, 255), game_engine.pg.Rect(30, 30, 60, 60))
+    # add animated object to map
+    bubbles_path = os.path.join(SPRITE_DIR, "bubbles", "bubbles0_0.png")
+    bubbles = AnimEntity(bubbles_path)
+    bubbles.rect.x = 20
+    bubbles.rect.y = 20
 
-    game_engine.pg.display.flip()
+    game_engine.entities.add(bubbles)
 
-    game_engine.main_loop()
+    # get status code while exiting main loop
+    exit_status = game_engine.main_loop()
+
+    if exit_status == PYGAME_SUCCESS:
+        logger.debug("Pygame engine exited successfully.")
+        sys.exit(PYGAME_SUCCESS)
+
+    elif exit_status == PYGAME_ERROR:
+        logger.warning("Pygame engine encountered an error.")
+        sys.exit(PYGAME_ERROR)
+
+    elif exit_status == PYGAME_FAILED:
+        logger.critical("Pygame engine encoutered a critical error"
+                        " and had to be terminated.")
+        sys.exit(PYGAME_FAILED)
