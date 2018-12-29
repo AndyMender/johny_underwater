9deb876c46a4636d6307d16c162456874634456b
Andy Mender <andymenderunix@gmail.com>
2018-12-29 15:40:40 +0100

    Fix test0 map - PyTMX supports only external tilesets

---
+++ assets/tilesets/test_area.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="test_area" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="../tiles/floor0.png" width="32" height="32"/>
+</tileset>
